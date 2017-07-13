38443a7c8a6d68c720c4655c7c39b125c640675d
Rozilio <hagayroz@gmail.com>
2017-07-14 00:08:20 +0300

    start

---
+++ JSON/API/Read.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API
+{
+    public class Read
+    {
+        private Read instance;
+        private Read()
+        {
+
+        }
+
+        public Read GetInstance()
+        {
+            if (instance == null)
+            {
+                Read instance = new Read();
+            }
+
+            return instance;
+        }
+
+        public static void ReadJson()
+        {
+
+        }
+
+        public static void Write()
+        {
+
+        }
+    }
+}
